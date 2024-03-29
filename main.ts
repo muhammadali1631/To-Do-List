5cce23039703bddecd6bbe99164470c7838d3fc8
muhammadali1631 <160423266+muhammadali1631@users.noreply.github.com>
2024-03-30 02:03:20 +0500

    First commit

---
+++ main.ts
@@ -0,0 +1,37 @@
+import inquirer from "inquirer"
+let todos = [];
+let condition = true;
+
+while(condition){
+let addTasks = await inquirer.prompt(
+    [
+        {
+            name: "todo",
+            type: "input",
+            message: "What do you want to add in your Todos?"
+        },
+        {
+            name: "addMore",
+            type: "confirm",
+            message: "Do you want to add More ?",
+            default: "false"
+        }
+    ]
+)
+todos.push(addTasks.todo)
+condition = addTasks.addMore
+console.log(todos)
+
+}
+
+// I made it myself
+let del = await inquirer.prompt({
+    name: "delete",
+    type: "confirm",
+    message: "Do you want to delete last value of your list?"
+})
+todos.pop()
+if(del.delete === true){
+
+    console.log(todos)
+}
